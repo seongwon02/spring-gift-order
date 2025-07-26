3b420e2d14d8ef109552bfee311111b9a046f80d
seongwon02 <seongwon2303@gmail.com>
2025-07-26 23:12:37 +0900

    refactor: KakaoTokenResponseDto를 record로 변환

--- src/main/java/gift/dto/kakao/KakaoTokenResponseDto.java
+++ src/main/java/gift/dto/kakao/KakaoTokenResponseDto.java
@@ -2,60 +2,23 @@ package gift.dto.kakao;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class KakaoTokenResponseDto {
+public record KakaoTokenResponseDto(
 
-    @JsonProperty("token_type") // JSON의 "token_type" 필드를 tokenType 필드에 매핑
-    private String tokenType;
+    @JsonProperty("token_type")
+    String tokenType,
 
     @JsonProperty("access_token")
-    private String accessToken;
+    String accessToken,
 
     @JsonProperty("expires_in")
-    private Integer expiresIn;
+    Integer expiresIn,
 
     @JsonProperty("refresh_token")
-    private String refreshToken;
+    String refreshToken,
 
     @JsonProperty("refresh_token_expires_in")
-    private Integer refreshTokenExpiresIn;
+    Integer refreshTokenExpiresIn,
 
-    private String scope;
-
-    public KakaoTokenResponseDto(String tokenType,
-                                 String accessToken,
-                                 Integer expiresIn,
-                                 String refreshToken,
-                                 Integer refreshTokenExpiresIn,
-                                 String scope) {
-        this.tokenType = tokenType;
-        this.accessToken = accessToken;
-        this.expiresIn = expiresIn;
-        this.refreshToken = refreshToken;
-        this.refreshTokenExpiresIn = refreshTokenExpiresIn;
-        this.scope = scope;
-    }
-
-    public String getTokenType() {
-        return tokenType;
-    }
-
-    public String getAccessToken() {
-        return accessToken;
-    }
-
-    public Integer getExpiresIn() {
-        return expiresIn;
-    }
-
-    public String getRefreshToken() {
-        return refreshToken;
-    }
-
-    public Integer getRefreshTokenExpiresIn() {
-        return refreshTokenExpiresIn;
-    }
-
-    public String getScope() {
-        return scope;
-    }
+    String scope
+    ){
 }
