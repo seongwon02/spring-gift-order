b089fa0b383716818bb140cfd28c25431cdb93c0
seongwon02 <seongwon2303@gmail.com>
2025-07-28 17:36:56 +0900

    feat: 카카오 토큰을 저장하는 엔티티와 토큰 재발급 시 사용하는 응답 DTO 구현

---
+++ src/main/java/gift/entity/MemberKakaoToken.java
@@ -0,0 +1,92 @@
+package gift.entity;
+
+import jakarta.persistence.*;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "member_kakao_token")
+public class MemberKakaoToken {
+
+    @Id
+    private Long memberId;
+
+    @MapsId
+    @OneToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "member_id")
+    private Member member;
+
+    @Column(nullable = false)
+    private String accessToken;
+
+    @Column(nullable = false)
+    private String refreshToken;
+
+    @Column(nullable = false)
+    private Instant accessTokenExpiresAt;
+
+    @Column(nullable = false)
+    private Instant refreshTokenExpiresAt;
+
+    protected MemberKakaoToken() {}
+
+    public MemberKakaoToken(Member member,
+                            String accessToken,
+                            String refreshToken,
+                            Instant accessTokenExpiresAt,
+                            Instant refreshTokenExpiresAt
+    ) {
+        this.member = member;
+        this.memberId = member.getId();
+        this.accessToken = accessToken;
+        this.refreshToken = refreshToken;
+        this.accessTokenExpiresAt = accessTokenExpiresAt;
+        this.refreshTokenExpiresAt = refreshTokenExpiresAt;
+    }
+
+    public void updateToken(String newAccessToken,
+                            String newRefreshToken,
+                            Instant newAccessExpiresAt,
+                            Instant newRefreshExpiresAt
+    ){
+        this.accessToken = newAccessToken;
+        this.accessTokenExpiresAt = newAccessExpiresAt;
+
+        if (newRefreshExpiresAt != null) {
+            this.refreshToken = newRefreshToken;
+            this.refreshTokenExpiresAt = newRefreshExpiresAt;
+        }
+    }
+
+    public boolean isAccessTokenExpired() {
+        return accessTokenExpiresAt.isBefore(Instant.now());
+    }
+
+    public boolean isRefreshTokenExpired() {
+        return refreshTokenExpiresAt.isBefore(Instant.now());
+    }
+
+    public Long getMemberId() {
+        return memberId;
+    }
+
+    public Member getMember() {
+        return member;
+    }
+
+    public String getAccessToken() {
+        return accessToken;
+    }
+
+    public String getRefreshToken() {
+        return refreshToken;
+    }
+
+    public Instant getAccessTokenExpiresAt() {
+        return accessTokenExpiresAt;
+    }
+
+    public Instant getRefreshTokenExpiresAt() {
+        return refreshTokenExpiresAt;
+    }
+}
